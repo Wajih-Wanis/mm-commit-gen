64d3f84c2254960f8e77c8d3024f2b7470bfff70
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-12-03 21:57:39 +0100

    Added interface for llms and implemented invokations

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const {Ollama} = require('@langchain/ollama');
+const {ChatOpenAI,AzureChatOpenAI} = require('@lanchain/openai');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -14,6 +16,11 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "mm-commit-gen" is now active!');
 
+
+	const AI = new LocalModel("llama3.2:1b");
+	const ollama_message = AI.run("Is ollama working ? ")
+	console.log(ollama_message);
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -26,6 +33,90 @@ function activate(context) {
 
 	context.subscriptions.push(disposable);
 }
+
+
+
+
+
+//abstract class model to interface model calling 
+class Model{
+	run(){
+		console.log('Running the interface invoke')
+	}
+}
+
+
+//class to run local model with ollama
+class LocalModel extends Model{
+	
+	constructor(model_name="llama3"){
+		super();
+		this.model = new Ollama({
+			model: model_name,
+		});
+	}
+	async run(input){
+		try{
+			const message = await this.model.invoke(input);
+			return message;
+		}catch(err){
+			console.error("Error in local model invokatio",err)
+			throw(err)
+		}
+	}
+}
+
+
+//class to run openai models
+class OpenaiModel extends Model{
+	
+	constructor(model){
+		super();
+		this.model = new ChatOpenAI({
+			model: model,
+			temperature: 0.5,
+		})
+	}
+
+	async run(input){
+		try{
+			const message = this.model.invoke(input);
+			return message;
+		}catch(err){
+			console.log("error occured",err)
+		}
+	}
+}
+
+
+//class to run azure openai deployments
+class AzureOpenaiModel extends Model{
+
+	constructor(endpoint,deployment,api_key,api_version){
+		super();
+		this.model = new AzureChatOpenAI({
+			temperature: 0.5,
+			azureOpenAIApiKey: api_key,
+			azureOpenAIApiInstanceName: endpoint,
+			azureOpenAIApiVersion:api_version,
+			azureOpenAIDeploymentName:deployment,
+		})
+	}
+
+	async run(input){
+		try{
+			const message = await this.model.invoke(input)
+			return message
+		}catch(err){
+			console.log("error occured",err)
+		}
+	}
+}
+
+
+
+
+
 
 // This method is called when your extension is deactivated
 function deactivate() {}
